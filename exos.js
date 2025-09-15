ede854e66a8ee0ad42a108d5fee71307eefa12a8
BriceHarper <58884670+BriceHarper@users.noreply.github.com>
2025-09-15 14:38:48 +0200

    exo1 to exo5

---
+++ exos.js
@@ -0,0 +1,22 @@
+// Exo 1
+let celcius = 20;
+console.log(`En celcius : ${celcius}. En fahrenheit : ${celcius * 9/5 + 32}`);
+
+// Exo 2
+let longueur = 20;
+let largeur = 15;
+let aireTriangle = longueur * largeur;
+console.log(aireTriangle);
+
+// Exo 3
+let prenom = "Brice";
+let nom = "Ramette";
+console.log(prenom + " " + nom);
+
+// Exo 4
+let prixHT = 20;
+const TVA = 0.2;
+console.log(`Montan HT : ${prixHT} Montant TVA : ${TVA} Montant TTC ${prixHT * (1+TVA)}`);
+
+// Exo 5
+console.log( ((4>=6) || ("herbe" != "verte")) && !(((12*2)==144)&& true) );
